92e40c026be83eb694b69db43872126655683439
tuananhhoang <montuananh@gmai.com>
2017-06-20 15:12:06 +0700

    Implement advanced login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   before_save :downcase_email
 
   validates :name, presence: true, length: {maximum: Settings.user.name_max_length}
@@ -19,6 +21,24 @@ class User < ApplicationRecord
       end
       BCrypt::Password.create string, cost: cost
     end
+
+    def new_token
+      SecureRandom.urlsafe_base64
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attributes remember_digest: User.digest(remember_token)
+  end
+
+  def forget
+    update_attributes remember_digest: nil
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+    BCrypt::Password.new(remember_digest).is_password? remember_token
   end
 
   private
